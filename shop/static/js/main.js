3081ca29e5da2c39e39ebc0762572df96c96ed0e
Milad Hatami <miladhzz@gmail.com>
2019-03-26 06:57:11 +0430

    use local js file in template

--- shop/static/js/main.js
+++ shop/static/js/main.js
@@ -85,7 +85,7 @@ jQuery(document).ready(function($){
         offset: 95
     })      
 });
-
+/*
   (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
   (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
   m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
@@ -93,3 +93,4 @@ jQuery(document).ready(function($){
 
   ga('create', 'UA-10146041-21', 'auto');
   ga('send', 'pageview');
+  */
